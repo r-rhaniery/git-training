f471479b01b7052a4c8d6cd5245721cd2aa67850
PColares <siqueira.pedro1998@gmail.com>
2021-07-30 11:10:36 -0300

    Add React's emojipedia project

---
+++ pedro/Projetos React/emojipedia/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
+import "/public/styles.css";
+
+ReactDom.render(<App />, document.querySelector("#root"));
