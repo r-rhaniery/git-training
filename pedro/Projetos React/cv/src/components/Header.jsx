e6685801a33c156a51ac1a7042e9742cac913f34
PColares <siqueira.pedro1998@gmail.com>
2021-08-03 18:47:00 -0300

    Add project cv

---
+++ pedro/Projetos React/cv/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <table cellSpacing="20">
+      <tr>
+        <td>
+            <span className = "alien">👽</span>
+        </td>
+        <td>
+          <h1 className="header">Pedro Colares</h1>
+          <p>Desenvolvedor Front-end</p>
+        </td>
+      </tr>
+    </table>
+  );
+}
+
+export default Header;
