1d9794a397b70614555aa68d21c6184aced80248
PColares <siqueira.pedro1998@gmail.com>
2021-08-11 17:33:01 -0300

    Add Categories to the project

--- pedro/curso-01/src/components/CardNota/CardNota.jsx
+++ pedro/curso-01/src/components/CardNota/CardNota.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import DeleteIcon from '@material-ui/icons/Delete';
+
 class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <DeleteIcon onClick={this.props.apagarNota} />
+          <h4>{this.props.categoria}</h4>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
