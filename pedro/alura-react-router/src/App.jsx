8bc9abe32f523cfeccea646b767c67660d4aad73
PColares <siqueira.pedro1998@gmail.com>
2021-08-24 09:31:10 -0300

    Update final version

--- pedro/alura-react-router/src/App.jsx
+++ pedro/alura-react-router/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import './assets/css/base/base.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
-import Page404 from './paginas/Page-404'
-import Header from './components/Header'
-import './assets/css/base/base.css'
-import './App.css';
+import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
+import Post from './paginas/Post'
+import Categoria from './paginas/Categoria'
 
 function App() {
+
   return (
     <Router>
-      <Header />
+      <Cabecalho />
       <Switch>
         <Route exact path='/'>
           <Home />
@@ -18,10 +20,16 @@ function App() {
         <Route path='/sobre'>
           <Sobre />
         </Route>
-        <Route>
-          <Page404 />
+        <Route path='/categoria/:id'>
+          <Categoria />
         </Route>
-      </ Switch>
+        <Route path='/posts/:id'>
+          <Post />
+        </Route>
+        <Route>
+          <Pagina404 />
+        </Route>
+      </Switch>
     </Router>
   )
 }
--- pedro/alura-react-router/src/api/api.js
+++ pedro/alura-react-router/src/api/api.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000'
+  baseURL: 'http://localhost:5000'
 })
 
-export const busca = async (url, setDado) => {
-    const reposta = await api.get(url)
-    setDado(reposta.data)
+export const busca = async(url, setDado) => { 
+  const reposta = await api.get(url)
+  setDado(reposta.data)
 }
